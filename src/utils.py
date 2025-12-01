3447ad4a423dbc5e62d29ef59f1244e310c23d08
Abir Hossain <abirfarabi915@gmail.com>
2025-12-01 23:15:57 +0000

    model train part 1

--- src/utils.py
+++ src/utils.py
@@ -14,3 +14,15 @@ def save_object(file_path, obj):
             dill.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try: 
+        report = {}
+
+        for name, mdl in models.items:
+            #model training
+            mdl.fit(X_train, y_train)
+
+            #prediction
+            y_train_pred = mdl.predict(X_train)
+            y_test_pred = mdl.predict(X_test)
